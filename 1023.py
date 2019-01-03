86ffe02dd3a506baf2550c6e67881144d4920884
BroJun <1406046462@qq.com>
2019-01-03 21:33:59 +0800

    1023

--- 1023.py
+++ 1023.py
@@ -1,2 +1,13 @@
-num = map(int,input().split())
-print(num)
+num = [int(x) for x in input().split()[:10]]
+strnum=''
+for x in ['0','1','2','3','4','5','6','7','8','9']:
+	strnum = strnum + x * num[int(x)]
+strnum = list(strnum)
+i = 1
+while True:
+	if strnum[0] != '0' : break
+	temp = strnum.pop(i)
+	strnum.insert(0,temp)
+	i += 1
+strnum = ''.join(strnum)
+print(strnum)
